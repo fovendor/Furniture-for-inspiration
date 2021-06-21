f275c455a46ec04cc0d1b58f573957edeef0caa4
Дмитрий <demgd3@gmail.com>
2021-06-21 15:23:25 +0500

    initial funio layout

---
+++ #src/js/files/script.js
@@ -0,0 +1,27 @@
+/* ------------------------------------------------------------------------------------------------------------------------ */
+/* ------------------------------- Обработчик нажатия для тачскринов после загрузки страницы ------------------------------ */
+/* ------------- Actions - делегивание события click. e - event. Получаем объект при клике и обрабатываем его ------------- */
+/* --------------------- isMobile.any возвращает true, если сайт просматривать с мобилки с тачскрином --------------------- */
+/* ---------------- После выполнения условий, получаем ближайшего родителя и меняем классы: нажат/не нажат ---------------- */
+/* ------------------ А потом пишем обработку, которая закрывает все подменю по событию клика в пустоту ------------------- */
+/* ------------------------------------------------------------------------------------------------------------------------ */
+
+window.onload = function () {
+  document.addEventListener("click", documentActions);
+
+  function documentActions (e) {
+    const targetElement = e.target;
+
+    if (window.innerWidth > 768 && isMobile.any()) {
+      if (targetElement.classList.contains('menu__arrow')) {
+        targetElement.closest('.menu__item').classList.toggle('_hover');
+        console.log('The user clicked on the arrow from a mobile device. Object "menu__item" assigned class _hover');
+      }
+      // проверяю на клик в пустоту. дословно : проверяю, есть ли вообще объекты с классом _hover.
+      if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+        // Если есть, то пишется функция, которая берёт коллекцию и удаляет выбранный класс у выбранных объектов из коллекции
+        _removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover");
+      }
+    }
+  }
+}
