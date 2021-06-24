bdbeb664b81c6b160bfe0238163384999a631a0b
Дмитрий <demgd3@gmail.com>
2021-06-24 13:28:05 +0500

    initial swiper

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -61,63 +61,27 @@ if (sliderScrollItems.length > 0) {
 
 function sliders_bild_callback(params) { }
 
-let slider_about = new Swiper('.about__slider', {
-	/*
-	effect: 'fade',
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	*/
-	observer: true,
-	observeParents: true,
-	slidesPerView: 1,
-	spaceBetween: 0,
-	autoHeight: true,
-	speed: 800,
-	//touchRatio: 0,
-	//simulateTouch: false,
-	//loop: true,
-	//preloadImages: false,
-	//lazy: true,
-	// Dotts
-	//pagination: {
-	//	el: '.slider-quality__pagging',
-	//	clickable: true,
-	//},
-	// Arrows
-	navigation: {
-		nextEl: '.about__more .more__item_next',
-		prevEl: '.about__more .more__item_prev',
-	},
-	/*
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
-		},
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 20,
-		},
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 20,
-		},
-		1268: {
-			slidesPerView: 4,
-			spaceBetween: 30,
-		},
-	},
-	*/
-	on: {
-		lazyImageReady: function () {
-			ibg();
-		},
-	}
-	// And if we need scrollbar
-	//scrollbar: {
-	//	el: '.swiper-scrollbar',
-	//},
-});
+if (document.querySelector('.slider-main__body')) {
+  new Swiper('slider-main__body', {
+    observer: true,   //
+    observerParent: true,   //
+    slidesPerView: 1,   //
+    spaceBetween: 32,   //px
+    wathOverflow: true,   //
+    speed: 600,   //
+    loop: true,   //
+    loopAdditionalSlides: 5,    //
+    preloadImages: false,   //
+    parallax: true,   // for link styles on the slide
+    // dotts
+    pagination: {
+      el: '.controls-slider-main__dotts',
+      clicable: true,
+    },
+    // arrows
+    navigation: {
+      nextEl: '.slider-main .slider-arrow-next',
+      prevEl: '.slider-main .slider-arrow-prev',
+    }
+  })
+}
--- Funiro/js/app.js
+++ Funiro/js/app.js
@@ -790,6 +790,94 @@ animate({
 			Element.prototype.msMatchesSelector;
 	}
 })();
+//BildSlider
+let sliders = document.querySelectorAll('._swiper');
+if (sliders) {
+	for (let index = 0; index < sliders.length; index++) {
+		let slider = sliders[index];
+		if (!slider.classList.contains('swiper-bild')) {
+			let slider_items = slider.children;
+			if (slider_items) {
+				for (let index = 0; index < slider_items.length; index++) {
+					let el = slider_items[index];
+					el.classList.add('swiper-slide');
+				}
+			}
+			let slider_content = slider.innerHTML;
+			let slider_wrapper = document.createElement('div');
+			slider_wrapper.classList.add('swiper-wrapper');
+			slider_wrapper.innerHTML = slider_content;
+			slider.innerHTML = '';
+			slider.appendChild(slider_wrapper);
+			slider.classList.add('swiper-bild');
+
+			if (slider.classList.contains('_swiper_scroll')) {
+				let sliderScroll = document.createElement('div');
+				sliderScroll.classList.add('swiper-scrollbar');
+				slider.appendChild(sliderScroll);
+			}
+		}
+		if (slider.classList.contains('_gallery')) {
+			//slider.data('lightGallery').destroy(true);
+		}
+	}
+	sliders_bild_callback();
+}
+
+function sliders_bild_callback(params) { }
+
+let sliderScrollItems = document.querySelectorAll('._swiper_scroll');
+if (sliderScrollItems.length > 0) {
+	for (let index = 0; index < sliderScrollItems.length; index++) {
+		const sliderScrollItem = sliderScrollItems[index];
+		const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
+		const sliderScroll = new Swiper(sliderScrollItem, {
+			observer: true,
+			observeParents: true,
+			direction: 'vertical',
+			slidesPerView: 'auto',
+			freeMode: true,
+			scrollbar: {
+				el: sliderScrollBar,
+				draggable: true,
+				snapOnRelease: false
+			},
+			mousewheel: {
+				releaseOnEdges: true,
+			},
+		});
+		sliderScroll.scrollbar.updateSize();
+	}
+}
+
+
+function sliders_bild_callback(params) { }
+
+if (document.querySelector('.slider-main__body')) {
+  new Swiper('slider-main__body', {
+    observer: true,   //
+    observerParent: true,   //
+    slidesPerView: 1,   //
+    spaceBetween: 32,   //px
+    wathOverflow: true,   //
+    speed: 600,   //
+    loop: true,   //
+    loopAdditionalSlides: 5,    //
+    preloadImages: false,   //
+    parallax: true,   // for link styles on the slide
+    // dotts
+    pagination: {
+      el: '.controls-slider-main__dotts',
+      clicable: true,
+    },
+    // arrows
+    navigation: {
+      nextEl: '.slider-main .slider-arrow-next',
+      prevEl: '.slider-main .slider-arrow-prev',
+    }
+  })
+}
+
 /* ------------------------------------------------------------------------------------------------------------------------ */
 /* ------------------------------- Обработчик нажатия для тачскринов после загрузки страницы ------------------------------ */
 /* ------------- Actions - делегивание события click. e - event. Получаем объект при клике и обрабатываем его ------------- */
